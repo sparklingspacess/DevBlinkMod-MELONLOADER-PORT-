481f600dbfa9460b9ba2c59913c1348f3f501adc
spark <streamerspark@gmail.com>
2025-05-04 07:27:56 +0100

    Update VRRigPatch.cs

--- DevBlinkMod/Scripts/Patches/VRRigPatch.cs
+++ DevBlinkMod/Scripts/Patches/VRRigPatch.cs
@@ -1,18 +1,1 @@
-﻿using DevBlinkMod.Scripts;
-using HarmonyLib;
-using System.Threading.Tasks;
-using UnityEngine;
-
-namespace DevBlinkMod.Scripts.Patches
-{
-    [HarmonyPatch(typeof(VRRig), "Start")]
-    class VRRigPatch
-    {
-        public static void VRRigPostfix(VRRig __instance)
-        {
-            GameObject instanceObject = __instance.gameObject;
-            if (instanceObject.GetComponent<BlinkManager>() != null && instanceObject.activeSelf) return;
-            instanceObject.AddComponent<BlinkManager>();
-        }
-    }
-}
+//this doesnt even work
