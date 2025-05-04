b3e4c107fc18427370cd8574846a69b9fefc4258
spark <streamerspark@gmail.com>
2025-05-04 07:02:09 +0100

    bad harmony patch
    probably buggy as hell but whatever im just gonna hope it doesnt cause a memory leak lmao

--- DevBlinkMod/Scripts/Patches/VRRigPatch.cs
+++ DevBlinkMod/Scripts/Patches/VRRigPatch.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 
 namespace DevBlinkMod.Scripts.Patches
 {
-    [HarmonyPatch(typeof(VRRig))]
-    internal class VRRigPatch
+    [HarmonyPatch(typeof(VRRig), "Start")]
+    class VRRigPatch
     {
-        [HarmonyWrapSafe]
-        [HarmonyPostfix]
-        [HarmonyPatch("Start")]
-        internal async static void VRRigPostfix(VRRig __instance)
+        public static void VRRigPostfix(VRRig __instance)
         {
-            await Task.Delay(500);
-
             GameObject instanceObject = __instance.gameObject;
             if (instanceObject.GetComponent<BlinkManager>() != null && instanceObject.activeSelf) return;
             instanceObject.AddComponent<BlinkManager>();
