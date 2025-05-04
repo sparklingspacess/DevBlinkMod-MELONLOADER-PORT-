c610a2725748904a48925f6b8e5b2659dd3c0f87
spark <streamerspark@gmail.com>
2025-05-04 06:52:37 +0100

    Update PluginInfo.cs

--- DevBlinkMod/PluginInfo.cs
+++ DevBlinkMod/PluginInfo.cs
@@ -1,12 +1,1 @@
-﻿namespace DevBlinkMod
-{
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
-    internal class PluginInfo
-    {
-        public const string GUID = "dev.blinkmod23";
-        public const string Name = "DevBlinkMod";
-        public const string Version = "1.1.2";
-    }
-}
+
