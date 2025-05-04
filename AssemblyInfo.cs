253ecb83fe959d291e12c3211c68c148d08d91e2
spark <streamerspark@gmail.com>
2025-05-04 07:38:08 +0100

    fixed assembly issue (how did i not notice this)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
 using DevBlinkMod;
 // ...
-[assembly: MelonInfo(typeof(Mod), "DevBlinkMod", "1.0.0", "developer9998")]
+[assembly: MelonInfo(typeof(Plugin), "DevBlinkMod", "1.0.0", "developer9998")]
 [assembly: MelonGame("Another Axiom", "Gorilla Tag")]
