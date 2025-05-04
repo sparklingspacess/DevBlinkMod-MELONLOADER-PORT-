790c1740058b61e4862c4ead5b893c2573b52a30
spark <streamerspark@gmail.com>
2025-05-04 06:21:10 +0100

    initial w.i.p port

--- DevBlinkMod/Plugin.cs
+++ DevBlinkMod/Plugin.cs
@@ -1,38 +1,40 @@
-﻿using BepInEx;
+//﻿using BepInEx;
 using DevBlinkMod.Scripts;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
+using MelonLoader;
 
 namespace DevBlinkMod
 {
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    public class Plugin : BaseUnityPlugin
+    public class Plugin : MelonMod
     {
         public static Plugin Instance { get; private set; }
+        private bool loaded;
         public Texture2D faceSheet;
 
-        internal async void Awake()
+        public override void OnSceneWasInitialized()
         {
-            Instance = this;
-            BlinkLogger.LogMessage("DevBlinkMod awoken", BlinkLogger.LogType.Default);
-
-            HarmonyPatches.ApplyHarmonyPatches();
-            BlinkLogger.LogMessage("Applied harmony patches", BlinkLogger.LogType.Default);
-
-            Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"DevBlinkMod.Resources.blinksheet.png");
-            byte[] bytes = new byte[manifestResourceStream.Length];
-            await manifestResourceStream.ReadAsync(bytes, 0, bytes.Length);
-
-            faceSheet = new Texture2D(192, 65, TextureFormat.RGB24, false)
+            if (!loaded)
             {
-                wrapMode = TextureWrapMode.Repeat,
-                filterMode = FilterMode.Point,
-                name = "Blink Sheet"
-            };
-            faceSheet.LoadImage(bytes);
-            faceSheet.Apply();
-            BlinkLogger.LogMessage("Generated face texture", BlinkLogger.LogType.Default);
+                loaded = true;
+                Instance = this;
+                BlinkLogger.LogMessage("DevBlinkMod awoken", BlinkLogger.LogType.Default);
+            
+                Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"DevBlinkMod.Resources.blinksheet.png");
+                byte[] bytes = new byte[manifestResourceStream.Length];
+                await manifestResourceStream.ReadAsync(bytes, 0, bytes.Length);
+
+                faceSheet = new Texture2D(192, 65, TextureFormat.RGB24, false)
+                {
+                    wrapMode = TextureWrapMode.Repeat,
+                    filterMode = FilterMode.Point,
+                    name = "Blink Sheet"
+                };
+                faceSheet.LoadImage(bytes);
+                faceSheet.Apply();
+                BlinkLogger.LogMessage("Generated face texture", BlinkLogger.LogType.Default);
+            }
         }
     }
 }
